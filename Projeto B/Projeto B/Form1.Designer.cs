0eb3d953ec2b7a05730841ff64c22370682e9aa3
walterISF <wisf_94@hotmail.com>
2015-09-02 20:58:51 -0300

    adicionando nome para os links
    formatando links

--- Projeto B/Projeto B/Form1.Designer.cs
+++ Projeto B/Projeto B/Form1.Designer.cs
@@ -30,63 +30,68 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
-            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
-            this.linkLabel3 = new System.Windows.Forms.LinkLabel();
+            this.LNK_login = new System.Windows.Forms.LinkLabel();
+            this.LNK_sobre = new System.Windows.Forms.LinkLabel();
+            this.LNK_sair = new System.Windows.Forms.LinkLabel();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(720, 30);
+            this.pictureBox1.Location = new System.Drawing.Point(540, 24);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(2, 700);
+            this.pictureBox1.Size = new System.Drawing.Size(2, 569);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // linkLabel1
+            // LNK_login
             // 
-            this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(771, 83);
-            this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(72, 17);
-            this.linkLabel1.TabIndex = 1;
-            this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "linkLabel1";
+            this.LNK_login.AutoSize = true;
+            this.LNK_login.Location = new System.Drawing.Point(578, 67);
+            this.LNK_login.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.LNK_login.Name = "LNK_login";
+            this.LNK_login.Size = new System.Drawing.Size(55, 13);
+            this.LNK_login.TabIndex = 1;
+            this.LNK_login.TabStop = true;
+            this.LNK_login.Text = "linkLabel1";
             // 
-            // linkLabel2
+            // LNK_sobre
             // 
-            this.linkLabel2.AutoSize = true;
-            this.linkLabel2.Location = new System.Drawing.Point(771, 115);
-            this.linkLabel2.Name = "linkLabel2";
-            this.linkLabel2.Size = new System.Drawing.Size(72, 17);
-            this.linkLabel2.TabIndex = 2;
-            this.linkLabel2.TabStop = true;
-            this.linkLabel2.Text = "linkLabel2";
+            this.LNK_sobre.AutoSize = true;
+            this.LNK_sobre.Location = new System.Drawing.Point(578, 93);
+            this.LNK_sobre.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.LNK_sobre.Name = "LNK_sobre";
+            this.LNK_sobre.Size = new System.Drawing.Size(55, 13);
+            this.LNK_sobre.TabIndex = 2;
+            this.LNK_sobre.TabStop = true;
+            this.LNK_sobre.Text = "linkLabel2";
             // 
-            // linkLabel3
+            // LNK_sair
             // 
-            this.linkLabel3.AutoSize = true;
-            this.linkLabel3.Location = new System.Drawing.Point(771, 148);
-            this.linkLabel3.Name = "linkLabel3";
-            this.linkLabel3.Size = new System.Drawing.Size(72, 17);
-            this.linkLabel3.TabIndex = 3;
-            this.linkLabel3.TabStop = true;
-            this.linkLabel3.Text = "linkLabel3";
+            this.LNK_sair.AutoSize = true;
+            this.LNK_sair.Location = new System.Drawing.Point(578, 120);
+            this.LNK_sair.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.LNK_sair.Name = "LNK_sair";
+            this.LNK_sair.Size = new System.Drawing.Size(55, 13);
+            this.LNK_sair.TabIndex = 3;
+            this.LNK_sair.TabStop = true;
+            this.LNK_sair.Text = "linkLabel3";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1182, 753);
-            this.Controls.Add(this.linkLabel3);
-            this.Controls.Add(this.linkLabel2);
-            this.Controls.Add(this.linkLabel1);
+            this.ClientSize = new System.Drawing.Size(888, 602);
+            this.Controls.Add(this.LNK_sair);
+            this.Controls.Add(this.LNK_sobre);
+            this.Controls.Add(this.LNK_login);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(1200, 800);
-            this.MinimumSize = new System.Drawing.Size(1200, 800);
+            this.MaximumSize = new System.Drawing.Size(904, 657);
+            this.MinimumSize = new System.Drawing.Size(904, 597);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SICAD";
@@ -99,9 +104,9 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.LinkLabel linkLabel1;
-        private System.Windows.Forms.LinkLabel linkLabel2;
-        private System.Windows.Forms.LinkLabel linkLabel3;
+        private System.Windows.Forms.LinkLabel LNK_login;
+        private System.Windows.Forms.LinkLabel LNK_sobre;
+        private System.Windows.Forms.LinkLabel LNK_sair;
 
 
     }
