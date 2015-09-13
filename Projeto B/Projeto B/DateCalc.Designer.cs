e30564449ef3ccd80a23b12eb0e0f1bfcc0d2f16
Aln-TheLord <allangera@hotmail.com>
2015-09-13 20:13:07 -0300

    Criando Form2 - DateCalc
    mudei uma propriedade na form1 que não deixa o usuario mudar o tamanho
    da form, sem ser a gambiarra que tava.

---
+++ Projeto B/Projeto B/DateCalc.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace Projeto_B
+{
+    partial class DateCalc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DateCalc));
+            this.SuspendLayout();
+            // 
+            // DateCalc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 453);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimizeBox = false;
+            this.Name = "DateCalc";
+            this.Text = "DateCalc";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
--- Projeto B/Projeto B/Form1.Designer.cs
+++ Projeto B/Projeto B/Form1.Designer.cs
@@ -34,10 +34,10 @@
             this.LNK_sobre = new System.Windows.Forms.LinkLabel();
             this.LNK_sair = new System.Windows.Forms.LinkLabel();
             this.GPB_login = new System.Windows.Forms.GroupBox();
-            this.TXT_login = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.TXT_senha = new System.Windows.Forms.TextBox();
+            this.TXT_login = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.GPB_login.SuspendLayout();
             this.SuspendLayout();
@@ -45,10 +45,10 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(600, 24);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Location = new System.Drawing.Point(800, 30);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(2, 568);
+            this.pictureBox1.Size = new System.Drawing.Size(3, 699);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
@@ -57,10 +57,9 @@
             this.LNK_login.AutoSize = true;
             this.LNK_login.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.LNK_login.LinkColor = System.Drawing.Color.Black;
-            this.LNK_login.Location = new System.Drawing.Point(622, 67);
-            this.LNK_login.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.LNK_login.Location = new System.Drawing.Point(829, 82);
             this.LNK_login.Name = "LNK_login";
-            this.LNK_login.Size = new System.Drawing.Size(51, 23);
+            this.LNK_login.Size = new System.Drawing.Size(66, 29);
             this.LNK_login.TabIndex = 1;
             this.LNK_login.TabStop = true;
             this.LNK_login.Text = "Login";
@@ -71,10 +70,9 @@
             this.LNK_sobre.AutoSize = true;
             this.LNK_sobre.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.LNK_sobre.LinkColor = System.Drawing.Color.Black;
-            this.LNK_sobre.Location = new System.Drawing.Point(622, 110);
-            this.LNK_sobre.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.LNK_sobre.Location = new System.Drawing.Point(829, 135);
             this.LNK_sobre.Name = "LNK_sobre";
-            this.LNK_sobre.Size = new System.Drawing.Size(55, 23);
+            this.LNK_sobre.Size = new System.Drawing.Size(70, 29);
             this.LNK_sobre.TabIndex = 2;
             this.LNK_sobre.TabStop = true;
             this.LNK_sobre.Text = "Sobre";
@@ -85,10 +83,9 @@
             this.LNK_sair.AutoSize = true;
             this.LNK_sair.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.LNK_sair.LinkColor = System.Drawing.Color.Black;
-            this.LNK_sair.Location = new System.Drawing.Point(622, 153);
-            this.LNK_sair.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.LNK_sair.Location = new System.Drawing.Point(829, 188);
             this.LNK_sair.Name = "LNK_sair";
-            this.LNK_sair.Size = new System.Drawing.Size(39, 23);
+            this.LNK_sair.Size = new System.Drawing.Size(50, 29);
             this.LNK_sair.TabIndex = 3;
             this.LNK_sair.TabStop = true;
             this.LNK_sair.Text = "Sair";
@@ -99,63 +96,69 @@
             this.GPB_login.Controls.Add(this.label1);
             this.GPB_login.Controls.Add(this.TXT_senha);
             this.GPB_login.Controls.Add(this.TXT_login);
-            this.GPB_login.Location = new System.Drawing.Point(654, 479);
+            this.GPB_login.Location = new System.Drawing.Point(872, 590);
+            this.GPB_login.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.GPB_login.Name = "GPB_login";
-            this.GPB_login.Size = new System.Drawing.Size(230, 120);
+            this.GPB_login.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.GPB_login.Size = new System.Drawing.Size(307, 148);
             this.GPB_login.TabIndex = 4;
             this.GPB_login.TabStop = false;
             this.GPB_login.Visible = false;
-            // 
-            // TXT_login
-            // 
-            this.TXT_login.Location = new System.Drawing.Point(56, 30);
-            this.TXT_login.Multiline = true;
-            this.TXT_login.Name = "TXT_login";
-            this.TXT_login.Size = new System.Drawing.Size(150, 25);
-            this.TXT_login.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(6, 30);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 19);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Login";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(6, 73);
+            this.label2.Location = new System.Drawing.Point(8, 90);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(48, 19);
+            this.label2.Size = new System.Drawing.Size(61, 24);
             this.label2.TabIndex = 2;
             this.label2.Text = "Senha";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 37);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Login";
+            // 
             // TXT_senha
             // 
-            this.TXT_senha.Location = new System.Drawing.Point(56, 73);
+            this.TXT_senha.Location = new System.Drawing.Point(75, 90);
+            this.TXT_senha.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.TXT_senha.Multiline = true;
             this.TXT_senha.Name = "TXT_senha";
-            this.TXT_senha.Size = new System.Drawing.Size(150, 25);
+            this.TXT_senha.Size = new System.Drawing.Size(199, 30);
             this.TXT_senha.TabIndex = 0;
+            // 
+            // TXT_login
+            // 
+            this.TXT_login.Location = new System.Drawing.Point(75, 37);
+            this.TXT_login.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.TXT_login.Multiline = true;
+            this.TXT_login.Name = "TXT_login";
+            this.TXT_login.Size = new System.Drawing.Size(199, 30);
+            this.TXT_login.TabIndex = 0;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(884, 601);
+            this.ClientSize = new System.Drawing.Size(1176, 730);
             this.Controls.Add(this.LNK_sair);
             this.Controls.Add(this.LNK_sobre);
             this.Controls.Add(this.LNK_login);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.GPB_login);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(900, 640);
-            this.MinimumSize = new System.Drawing.Size(900, 640);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SICAD";
